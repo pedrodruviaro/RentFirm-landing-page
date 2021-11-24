1e12a03120306cf1a04c5732fc814bd754c1f2a8
Pedro Dalmolin <pedrodruviaro@gmail.com>
2021-11-24 14:09:44 -0300

    add: assets, defaultContainer, Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
-import { Button } from "./components/Button";
+import { Home } from "./pages/Home";
 
-export const App = () => {
-    return (
-        <>
-            <h1>React App</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et sunt
-                enim laudantium nobis molestiae, velit consectetur a vitae
-                aperiam eum excepturi nesciunt quisquam veniam, dolorem
-                voluptate perspiciatis voluptates temporibus magnam.
-            </p>
-            <Button>More About Us</Button>
-        </>
-    );
-};
+export const App = () => <Home />;
---
+++ src/components/DefaultContainer/index.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+    max-width: 68.75rem;
+
+    margin: 2.5rem auto 0 auto;
+    padding: 1.5rem;
+
+    @media (min-width: 40rem) {
+        padding: 2rem;
+    }
+
+    @media (min-width: 55rem) {
+        padding: 2.5rem;
+    }
+`;
