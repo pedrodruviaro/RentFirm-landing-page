72ca180d80be854ae7a6253559b97dd13e97783d
Pedro Dalmolin <pedrodruviaro@gmail.com>
2021-11-24 13:33:52 -0300

    structuring project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export const App = () => {
+    return (
+        <>
+            <h1>React App</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et sunt
+                enim laudantium nobis molestiae, velit consectetur a vitae
+                aperiam eum excepturi nesciunt quisquam veniam, dolorem
+                voluptate perspiciatis voluptates temporibus magnam.
+            </p>
+            <button>More About Us</button>
+        </>
+    );
+};
---
+++ src/common/Theme.jsx
@@ -0,0 +1,11 @@
+export const Theme = {
+    light: "#FFFFFF",
+    medium: "#5A6473",
+    dark: "#101010",
+
+    accent: "#0689FF",
+    accentLight: "#0689FF",
+    accentDark: "#0F1B4C",
+
+    heading: "#000339",
+};
